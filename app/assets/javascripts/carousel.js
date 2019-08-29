efd52e333ea707e2a52614feae3824a71aa1f24c
PaulMouchel <paulmouchel@live.fr>
2019-08-29 12:19:46 +0200

    commit test

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,40 +1,40 @@
-$(document).ready(function() {
-	let displayedImg = 0;
-	let max = $("#carousel").children().length - 1;
-	display();
+// $(document).ready(function() {
+// 	let displayedImg = 0;
+// 	let max = $("#carousel").children().length - 1;
+// 	display();
 
-	function display() {
-		$(".carousel-img").hide();
-		$("#carousel").children().eq(displayedImg).fadeIn(500);
-		clearDots();
-		$(".dot").eq(displayedImg).addClass("dot-active")
-	}
+// 	function display() {
+// 		$(".carousel-img").hide();
+// 		$("#carousel").children().eq(displayedImg).fadeIn(500);
+// 		clearDots();
+// 		$(".dot").eq(displayedImg).addClass("dot-active")
+// 	}
 
-	$("#previous").click(function(){
-		if (--displayedImg < 0)
-			displayedImg = max;
-		display();
-	});
+// 	$("#previous").click(function(){
+// 		if (--displayedImg < 0)
+// 			displayedImg = max;
+// 		display();
+// 	});
 
-	function next() {
-		if (++displayedImg > max)
-			displayedImg = 0;
-		display();
-	}
+// 	function next() {
+// 		if (++displayedImg > max)
+// 			displayedImg = 0;
+// 		display();
+// 	}
 
-	$("#next").click(next);
+// 	$("#next").click(next);
 
-	window.setInterval(next, 3000); 
+// 	window.setInterval(next, 3000); 
 
-	$(".dot").click(function(){
-		displayedImg = $(this).attr("slide");
-		display();
-	});
+// 	$(".dot").click(function(){
+// 		displayedImg = $(this).attr("slide");
+// 		display();
+// 	});
 
-	function clearDots() {
-		for(i=0 ; i<=max ; i++) {
-			$(".dot").eq(i).removeClass("dot-active");
-		}
-	}
-});
+// 	function clearDots() {
+// 		for(i=0 ; i<=max ; i++) {
+// 			$(".dot").eq(i).removeClass("dot-active");
+// 		}
+// 	}
+// });
 
